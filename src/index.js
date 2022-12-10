32850347c54991936496fc4f8175ee7c664b19ee
TaylorWhite21 <taylorwhite2190@gmail.com>
2022-12-10 12:54:37 -0500

    Changed index.js to be more reactive based on window width

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+function MyApp() {
+    // Use the useState hook to track the width of the window
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+    // Add an event listener for the resize event
+    window.addEventListener('resize', function() {
+        // Update the window width state when the event is triggered
+        setWindowWidth(window.innerWidth);
+    });
+
+    return (
+        // Pass the window width as a prop to the App component
+        <App windowWidth={windowWidth} />
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyApp />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
